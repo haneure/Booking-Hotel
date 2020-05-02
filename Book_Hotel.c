70c6b387430780586dcf289ca432bab91690e83d
Juliushoho <juliuscannavaro11@gmail.com>
2020-05-02 15:55:17 +0700

    banana chris

--- Book_Hotel.c
+++ Book_Hotel.c
@@ -7,7 +7,7 @@
 #include <windows.h>
 #include <conio.h>
 
-
+banana
 
 struct bookv{           //Kedua struct ini berbeda karena kami gunakan untuk membedakan kamar hotel VIP dan Reguler
     char nama[30];      //Struct bookv untuk VIP
