cad16c1c72bbcb26d884846006ebca5ced6f142b
Chris <chris.richard.halim@gmail.com>
2020-05-02 15:06:07 +0700

    ilangin line 10

--- Book_Hotel.c
+++ Book_Hotel.c
@@ -7,7 +7,6 @@
 #include <windows.h>
 #include <conio.h>
 
-awa
 
 struct bookv{           //Kedua struct ini berbeda karena kami gunakan untuk membedakan kamar hotel VIP dan Reguler
     char nama[30];      //Struct bookv untuk VIP
