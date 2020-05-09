08c3af635abc72ed27d8a0ef4fc252f73f2e3885
Evan <l12181698@gmail.com>
2020-05-09 14:31:20 +0700

    Test DOang ini abaiokan

--- Book_Hotel.c
+++ Book_Hotel.c
@@ -7,7 +7,7 @@
 #include <windows.h>
 #include <conio.h>
 
-
+Test dooang
 struct bookv{           //Kedua struct ini berbeda karena kami gunakan untuk membedakan kamar hotel VIP dan Reguler
     char nama[30];      //Struct bookv untuk VIP
     char code [100];
